d2516f257a4542d99bce56f5bf93d55a26e650fe
Mathew Desjardins <desjardinsm@etsu.edu>
2022-03-01 14:40:50 -0500

    Add project files.

---
+++ SqliteExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Data.Sqlite;
+using SqliteExample;
+
+var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+
+
+using (var connection = new SqliteConnection($@"Data Source={path}{Path.DirectorySeparatorChar}mydatabase.db"))
+{
+    connection.Open();
+
+    
+    var command = connection.CreateCommand();
+
+    command.CommandText = "select * from User";
+
+    var data = command.ExecuteReader();
+
+    Console.WriteLine(data.FieldCount);
+
+    var users = new List<User>();
+
+    while (data.Read())
+    {
+        var dict = new Dictionary<string, object>();
+
+        for (int i = 0; i < data.FieldCount; i++)
+        {
+
+            dict.Add(data.GetName(i), data.GetValue(i));
+        }
+
+        users.Add(new User((long)(dict["Id"]), (string)dict["Name"]));
+  
+       
+    }
+
+    foreach (var item in users)
+    {
+        Console.WriteLine(item.Name);
+    }
+
+    
+
+    /*for (int i = 0; i < 50; i++)
+    {
+        command.CommandText = "INSERT INTO User (Name) VALUES ('Cory')";
+
+        command.ExecuteNonQuery();
+    }*/
+    
+
+    //command.Parameters.AddWithValue("$id", id);
+
+   /* using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            var name = reader.GetString(0);
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }*/
+}
---
+++ SqliteExample/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteExample
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }   
+
+
+        public User(long id, string name)
+        {
+            Id = id;
+            Name = name;
+
+        }
+    }
+}
